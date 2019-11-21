8dc98142b900190f7e1e51cb18445f675312d818
karolienpeeters <karpee@bitconsult.be>
2019-11-21 14:35:33 +0100

    token expires aangepast

--- FullStack.BLL/Services/UserService.cs
+++ FullStack.BLL/Services/UserService.cs
@@ -40,7 +40,7 @@ namespace FullStack.BLL.Services
                         "https://localhost:44318",
                         "*",
                         claims,
-                        expires: DateTime.Now.AddMinutes(1),
+                        expires: DateTime.Now.AddMinutes(120),
                         signingCredentials: signinCredentials
                     );
                     var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
